69d8ac5011ca117bb8ea361216f2ce8060f7aeae
asugar13 <asier.ugarteche@gmail.com>
2020-05-26 19:40:12 -0400

    deleted redundant files

--- user_routes.js
+++
@@ -1,8 +0,0 @@
-exports.SignUp = function(req, res) {
-  console.log("sup");
-  //var user_name = req.body.user;
-  //console.log(user_name);
-  console.log(req);
-  console.log(req.body.user);
-
-}
